f96068302e486eba00f204372900ddcca189c371
sebauribeo <chefsebauribe@gmail.com>
2020-11-09 23:34:19 -0300

    integracion grafico canvasJs

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,28 +10,67 @@ $(document).ready(function(){
             success: function (response) {
                 console.log(response);
                 let character = response;
-                $('#pokemon').html(`
+                
+                $('#pokemon-1').html(`
+                    <h4>${character.name}</h4>
                     <img src="${character.sprites.front_default}" alt="">
-                    <p>Nombre: ${character.name}</p>
-                    <p>Tamaño: ${character.weight} CM</p>
-                    <p>N° Pokedex: ${character.id} </p>
-                    `);
+                `);
+                
+                var chart = new CanvasJS.Chart("chartContainer", {
+               
+                    backgroundColor: '#9ae4ed',
+                    animationEnabled: true,
+                    title: {
+                        text: 'Stats',
+                        fontWeight:'bolder',
+                        padding:10,
+                        backgroundColor: '#ffff00',
+                        cornerRadius:15,
+                    },
+                    data: [{
+                        type: "pie",
+                        startAngle: 240,
+                        fontColor: '#000000',
+                        fontWeight:'bolder',
+                        yValueFormatString: "##\"",
+                        indexLabel: "{label} {y}",
+                        dataPoints: [
+                            {y: character.stats[0].base_stat, label: "HP"},
+                            {y: character.stats[1].base_stat, label: "Attack"},
+                            {y: character.stats[2].base_stat, label: "Defense"},
+                            {y: character.stats[3].base_stat, label: "Sp. Attack"},
+                            {y: character.stats[4].base_stat, label: "Sp. Defense"},
+                            {y: character.stats[5].base_stat, label: "Speed"},
 
+                        ]
+                    }]
+                });
+                chart.render();
+                
+                $('#pokemon-2 > ul').html(`
+                    <h4>Caracteristicas:</h4>
+                    <h5>Tamaño:</h5>
+                    <p>${character.weight} CM.</p>
+                    <h5>N° Pokedex: </h5>
+                    <p>${character.id} </p>
+                    <h5>Tipo:</h5>
+                `);
+                character.types.map((tipo) =>{
+                    $('#pokemon-2 > ul').append(`            
+                    <li>${tipo.type.name}</li>
+                    `);
+                });
+                
                 $('input').val("");
+
             },
             error: function(error){
                 console.error(error);
-                $('#pokemon').html(`
-                <p>El Nombre ( ${entrada} ) que buscas no existe, intenta nuevamente</p>
+                $('#chartContainer').html(`
+                <p>El Nombre (${entrada}) que buscas no existe, intenta nuevamente</p>
             `)
-            $('input').val("");
-
             }
-
-            
         });
-    
     });
-
 });
 
